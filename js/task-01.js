6ae3c2e2ae63e356fe5e86966711c1f07f2ac002
Maxim Moskalik <90215347+Moscowalik@users.noreply.github.com>
2022-01-12 22:35:22 +0200

    Delete js directory

--- js/task-01.js
+++
@@ -1,8 +0,0 @@
-const categoriesRef = document.querySelector('#categories')
-
-console.log('Number of categories:' , categoriesRef.childElementCount)
-
-const elementsOfCategoriesRef = [...categoriesRef.children].map(element => {
-    console.log('Category:', element.firstElementChild.textContent),
-    console.log ('Elements:', element.lastElementChild.children.length)})
-
--- js/task-05.js
+++
@@ -1,9 +0,0 @@
-const inputRef = document.querySelector('#name-input')
-const spanRef = document.querySelector('#name-output')
-
-
-inputRef.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    spanRef.textContent = inputRef.value === '' ? 'Anonymous' : event.currentTarget.value;
-}
