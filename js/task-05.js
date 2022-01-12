db007746156fd9f7aca63d0dfdcdc7b94c57ec49
Maxim Moskalik <90215347+Moscowalik@users.noreply.github.com>
2022-01-12 19:44:29 +0200

    Delete js directory

--- js/task-05.js
+++
@@ -1,9 +0,0 @@
-const inputRef = document.querySelector('#name-input')
-const spanRef = document.querySelector('#name-output')
-console.log(inputRef)
-
-inputRef.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    spanRef.textContent = event.currentTarget.value
-}
